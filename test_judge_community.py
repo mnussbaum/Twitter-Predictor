e97b297251534f54bf5b73b1f5920fc2d3cdaffa
Michael Nussbaum <michaelnussbaum08@gmail.com>
2010-11-16 20:54:32 -0600

    changes for testing

--- test_judge_community.py
+++ test_judge_community.py
@@ -8,13 +8,14 @@ from stats import PopulationStats
 from utils import write_output, read_output
 
 def main():
-    community = read_output('pickled_populations/testing_community_ids')
+    community = read_output('pickled_populations/jerwill')
     s = PopulationStats(community['members'])
     community_member_names = s.all_user_names()
     print 'Community Members:', len(community['members'])
     for user in community['members']:
         print ''
         print 'User:', user['screen_name']
+        print 'ID:', user['uid']
         print 'Tweet Count:', len(user['tweets'])
         print 'Friend IDs:', len(user['friend_ids'])
         print 'Follower IDs:', len(user['follower_ids'])
