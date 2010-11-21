2d90118099a450ed191c81acb2009ed5f0e037d0
Michael Nussbaum <michaelnussbaum08@gmail.com>
2010-11-20 19:50:06 -0600

    added logging

--- test_get_community.py
+++ test_get_community.py
@@ -1,24 +1,24 @@
 #! /usr/bin/python
+import logging
 from time import sleep
 from datetime import datetime
 from pprint import PrettyPrinter
 pp = PrettyPrinter(indent=4)
 from utils import write_output
 from populate import Population
+from __init__ import LOG_FILENAME
+
+logging.basicConfig(filename=LOG_FILENAME,level=logging.DEBUG, 
+  format='%(asctime)s %(name)-12s %(levelname)-8s %(message)s',
+  datefmt='%m-%d %H:%M')
 
 def main():
-    #first round of gathering
-    community = get_community(60919433, write=True, \
-      max_people=200, max_followers_per_person=5, out_file="lizardbill_11_20_2010", \
+    community = get_community(60919433, write=True,
+      max_people=200, max_followers_per_person=5, out_file="lizardbill_11_20_2010",
       write_dir="pickled_populations/", new_community=False, safe_mode=False)
-    #print 'DONE ROUND ONE'
-    #second round of gathering 
-    #community = get_community(49893981, max_people=10, \
-    #  write=True, out_file="testing_community_ids", \
-    #  write_dir="pickled_populations/", new_community=False)
-    #print 'DONE ROUND TWO'
+   
 
-def get_community(root_id, max_people=10, max_followers_per_person=2,\
+def get_community(root_id, max_people=10, max_followers_per_person=2,
    write=False, out_file="", write_dir="", new_community=True, safe_mode=False):
     if write:
         if out_file:
@@ -32,13 +32,14 @@ def get_community(root_id, max_people=10, max_followers_per_person=2,\
             write_path = file_name
     else:
         write_path = None
-    pop = Population(root_id, max_people, max_followers_per_person, \
+    pop = Population(root_id, max_people, max_followers_per_person,
       write_path, new=new_community, safe=safe_mode)
     try:
         pop.populate()
         community = pop.get_community()
 	return community
     except:
+        logging.debug('Uncaught exception, quitting')
         pop.save()
     
 if __name__=="__main__":
