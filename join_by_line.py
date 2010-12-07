430f47608634a333b6753ff06cbefd18679cb97d
Jeffrey Schecter <jeffrey.schecter@gmail.com>
2010-12-07 14:56:43 -0600

    joins two files by line

---
+++ join_by_line.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+
+# Takes two files, f1 and f2. For each line index i, prints f1[i] \t f2[i]
+# Stops when the shorter file runs out of lines.
+
+import sys
+import getopt
+
+def main():
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], "f:g:o:h")
+    except getopt.GetoptError:
+        print "Invalid options. Valid options are -f FILE1 -f FILE2 -o OUTPATh -h"
+        sys.exit(1)
+    in1 = None
+    in2 = None
+    outpath = None
+    head = False
+    for o, a in opts:
+        if o == '-f':
+            in1 = a
+        if o == '-g':
+            in2 = a
+        if o == '-o':
+            outpath = a
+        if o == '-h':
+            head = True
+    f1 = open(in1, 'r')
+    f2 = open(in2, 'r')
+    lines1 = f1.readlines()
+    lines2 = f2.readlines()
+    f1.close()
+    f2.close()
+    outfile = open(outpath, 'w')
+    L = min(len(lines1), len(lines2))
+    for i in range(L):
+        one = lines1[i].rstrip('\n')
+        two = lines2[i]
+        output = one + '\t' + two
+        outfile.write()
