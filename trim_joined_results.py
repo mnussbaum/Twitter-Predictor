6a54f1e19e491a2f61f2307cd17e0e3bc99c2bea
Michael Nussbaum <michaelnussbaum08@gmail.com>
2010-12-07 17:34:07 -0600

    transform joined word data into a form suitable for graphing

---
+++ trim_joined_results.py
@@ -0,0 +1,24 @@
+import sys
+
+def main(file_path):
+    f = open(file_path, 'r')
+    lines = f.readlines()
+    f.close()
+    lines_within_bounds = []
+    for line in lines:
+        split_line = line.split()
+        if float(split_line[-1]) < 1000:
+            lines_within_bounds.append(line)        
+    #take ever 100th line
+    desired_output = []
+    line_counter = 0
+    for line in lines_within_bounds:
+       if line_counter % 100 == 0:
+           desired_output.append(line)
+       line_counter += 1
+    f = open(file_path, 'w')
+    f.write(''.join(desired_output))
+    f.close()
+
+if __name__ == "__main__":
+    main(sys.argv[1])
